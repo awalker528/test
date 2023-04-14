30e8969f23bca37410a19f159b9fedf635142f98
awalker528 <120226068+awalker528@users.noreply.github.com>
2023-04-13 21:23:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+async function bibleVerse(){
+   await fetch("https://bible-api.com/john 3:16")
+    .then(response => text = response.json())
+    .then(text => verse = text.text)
+    .then(verse => document.querySelector("#newData").innerHTML = verse.toString())
+    .then(verse => console.log(verse))
+    
 
-await fetch("https://bible-api.com/john 3:16")
-.then(response => text = response.json())
-.then(text => verse = text.text)
-.then(verse => console.log(verse))
-.then(verse => document.querySelector("#newData").innerText = verse.toString())
+}
