94e2241ed11ffc6f6895ac0093638f8facde1499
awalker528 <120226068+awalker528@users.noreply.github.com>
2023-04-13 21:18:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-fetch("https://bible-api.com/john 3:16")
+await fetch("https://bible-api.com/john 3:16")
 .then(response => text = response.json())
 .then(text => verse = text.text)
 .then(verse => console.log(verse))
